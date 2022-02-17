a073c9e79e76d3b5e9a975b7ac5576ffc78be9e9
TeleOniOn <44299448+TeleOniOn@users.noreply.github.com>
2022-02-17 10:35:02 -0800

    Add files via upload

--- utlis/locks.py
+++ utlis/locks.py
@@ -175,7 +175,7 @@ def st(client, message,redis,type = 1):
     reply_markup=InlineKeyboardMarkup([
         [
           InlineKeyboardButton(link,callback_data=json.dumps(["LU","Llink",userID,1])),
-          InlineKeyboardButton(bots,callback_data=json.dumps(["LU","Lbots",userID,1])),
+          InlineKeyboardButton(bots,callback_data=json.dumps(["LU","Lbots",userID,4])),
           ],[
             InlineKeyboardButton(video,callback_data=json.dumps(["LU","Lvideo",userID,1])),
             InlineKeyboardButton(note,callback_data=json.dumps(["LU","Lnote",userID,1])),
@@ -201,7 +201,7 @@ def st(client, message,redis,type = 1):
             InlineKeyboardButton(contact,callback_data=json.dumps(["LU","Lcontact",userID,2])),
             ],[
               InlineKeyboardButton(sticker,callback_data=json.dumps(["LU","Lsticker",userID,2])),
-              InlineKeyboardButton(fwd,callback_data=json.dumps(["LU","Lfwd",userID,2])),
+              InlineKeyboardButton(fwd,callback_data=json.dumps(["LU","Lfwd",userID,4])),
               ],[
                 InlineKeyboardButton(username,callback_data=json.dumps(["LU","Lusername",userID,2])),
                 InlineKeyboardButton(english,callback_data=json.dumps(["LU","Lenglish",userID,2])),
@@ -225,7 +225,7 @@ def st(client, message,redis,type = 1):
                     InlineKeyboardButton(tag,callback_data=json.dumps(["LU","Ltag",userID,3])),
           ],[
                     InlineKeyboardButton(join,callback_data=json.dumps(["LU","Ljoin",userID,3])),
-                    InlineKeyboardButton(edits,callback_data=json.dumps(["LU","Ledits",userID,3])),
+                    InlineKeyboardButton(edits,callback_data=json.dumps(["LU","Ledits",userID,4])),
             ],[
                     InlineKeyboardButton(longtext,callback_data=json.dumps(["LU","Llongtext",userID,3])),
               ],t
